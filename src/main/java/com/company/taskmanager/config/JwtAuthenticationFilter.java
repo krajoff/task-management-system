adf0bff89446816113ccff3843b1d87ee59102cb
krajoff <t0k@bk.ru>
2024-08-21 15:39:15 +0300

    add javacode jwt and global exception

--- src/main/java/com/company/taskmanager/config/JwtAuthenticationFilter.java
+++ src/main/java/com/company/taskmanager/config/JwtAuthenticationFilter.java
@@ -20,15 +20,47 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+/**
+ * JwtAuthenticationFilter — это фильтр, который обрабатывает каждый входящий HTTP-запрос,
+ * извлекая и проверяя JWT-токен из заголовка Authorization. Если токен валиден, фильтр
+ * аутентифицирует пользователя и устанавливает контекст безопасности для дальнейшей обработки.
+ *
+ * <p>Этот фильтр является частью цепочки фильтров Spring Security и выполняется один раз за запрос.
+ * Он использует {@link JwtService} для обработки токена (его извлечения и проверки) и {@link UserService}
+ * для загрузки данных пользователя.</p>
+ *
+ * <p>Если токен отсутствует или недействителен, фильтр передаёт запрос дальше по цепочке,
+ * не изменяя контекст безопасности. Если токен валиден, он аутентифицирует пользователя и
+ * устанавливает аутентификацию в {@link SecurityContextHolder}.</p>
+ */
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    /**
+     * Префикс для JWT-токена в заголовке Authorization.
+     */
     public static final String BEARER_PREFIX = "Bearer ";
+
+    /**
+     * Название HTTP-заголовка, содержащего JWT-токен.
+     */
     public static final String HEADER_NAME = "Authorization";
+
     private final JwtService jwtService;
     private final UserService userService;
 
-
+    /**
+     * Фильтрует входящие запросы, проверяя наличие JWT-токена в заголовке Authorization.
+     * Если токен найден и валиден, происходит извлечение данных пользователя и его аутентификация.
+     *
+     * @param request     HTTP-запрос
+     * @param response    HTTP-ответ
+     * @param filterChain цепочка фильтров для передачи запроса дальше
+     * @throws ServletException если возникает ошибка при фильтрации
+     * @throws IOException      если возникает ошибка ввода/вывода при фильтрации
+     * @throws JwtAuthException если JWT-токен недействителен
+     */
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
--- src/main/java/com/company/taskmanager/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/company/taskmanager/exceptions/GlobalExceptionHandler.java
@@ -1,16 +1,32 @@
 package com.company.taskmanager.exceptions;
 
 import com.company.taskmanager.models.errors.AppError;
-import io.jsonwebtoken.ExpiredJwtException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * GlobalExceptionHandler — это глобальный обработчик исключений, который перехватывает
+ * различные исключения в приложении и возвращает соответствующие HTTP-ответы с информацией
+ * об ошибке и статусами.
+ *
+ * <p>Этот класс использует аннотацию {@link ControllerAdvice} для глобального перехвата
+ * исключений, выбрасываемых в контроллерах. Каждое исключение обрабатывается
+ * соответствующим методом, который возвращает объект {@link ResponseEntity}, содержащий
+ * информацию об ошибке в виде {@link AppError} и HTTP-статус.</p>
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    /**
+     * Обрабатывает исключение {@link ResourceNotFoundException}, которое
+     * возникает, когда запрашиваемый ресурс не найден.
+     *
+     * @param e исключение {@link ResourceNotFoundException}
+     * @return ответ с кодом 404 NOT FOUND и сообщением об ошибке
+     */
     @ExceptionHandler
     public ResponseEntity<AppError> catchResourceNotFoundException
             (ResourceNotFoundException e) {
@@ -18,6 +34,14 @@ public class GlobalExceptionHandler {
                 new AppError(HttpStatus.NOT_FOUND.value(),
                         e.getMessage()), HttpStatus.NOT_FOUND);
     }
+
+    /**
+     * Обрабатывает исключение {@link AuthException}, которое
+     * возникает при ошибке аутентификации.
+     *
+     * @param e исключение {@link AuthException}
+     * @return ответ с кодом 401 UNAUTHORIZED и сообщением об ошибке
+     */
     @ExceptionHandler
     public ResponseEntity<AppError> catchAuthException
             (AuthException e) {
@@ -26,6 +50,13 @@ public class GlobalExceptionHandler {
                         e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
+    /**
+     * Обрабатывает исключение {@link JwtAuthException}, которое возникает
+     * при ошибке проверки JWT-токена.
+     *
+     * @param e исключение {@link JwtAuthException}
+     * @return ответ с кодом 401 UNAUTHORIZED и сообщением об ошибке
+     */
     @ExceptionHandler
     public ResponseEntity<AppError> catchJwtAuthException
             (JwtAuthException e) {
@@ -34,6 +65,14 @@ public class GlobalExceptionHandler {
                         e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
+    /**
+     * Обрабатывает исключение {@link JwtExpiredException}, которое возникает,
+     * когда срок действия JWT-токена истек.
+     *
+     * @param e исключение {@link JwtExpiredException}
+     * @return ответ с кодом 401 UNAUTHORIZED и сообщением об истечении срока
+     * действия токена
+     */
     @ExceptionHandler
     public ResponseEntity<AppError> catchJwtExpiredException
             (JwtExpiredException e) {
@@ -42,6 +81,13 @@ public class GlobalExceptionHandler {
                         e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
+    /**
+     * Обрабатывает исключение {@link UsernameNotFoundException},
+     * которое возникает, если пользователь не найден.
+     *
+     * @param e исключение {@link UsernameNotFoundException}
+     * @return ответ с кодом 404 NOT FOUND и сообщением об ошибке
+     */
     @ExceptionHandler
     public ResponseEntity<AppError> catchUserNotFoundException
             (UsernameNotFoundException e) {
